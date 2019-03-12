b491be9b7a8e138af3a174b98c729658d15c4c8d
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-12 16:16:37 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 #include <math.h>
 #include "ukf.h"
 #include "tools.h"
-
 using namespace std;
-
 // for convenience
 using json = nlohmann::json;
 
@@ -14,16 +12,16 @@ using json = nlohmann::json;
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
 std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("]");
-  if (found_null != std::string::npos) {
-    return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
+	auto found_null = s.find("null");
+	auto b1 = s.find_first_of("[");
+	auto b2 = s.find_first_of("]");
+	if (found_null != std::string::npos) {
+	return "";
+	} else if (b1 != std::string::npos && b2 != std::string::npos) {
+		return s.substr(b1, b2 - b1 + 1);
+	}
+	
+	return "";
 }
 
 int main()
